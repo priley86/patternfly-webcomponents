c1c0f6e8b98f47f2c8c4800ecd246be406d7c5cf
sahil143 <sahilbudhwar143@gmail.com>
2017-02-07 13:56:58 +0530

    dropdown first commit

---
+++ src/pf-dropdown/pf-dropdown.template.js
@@ -0,0 +1,10 @@
+var pfDropdownTemplate = `
+<div class="dropdown">
+  <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown<span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu">
+  </ul>
+</div>
+`
+export{ pfDropdownTemplate as default}
