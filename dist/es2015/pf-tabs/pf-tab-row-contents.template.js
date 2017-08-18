b254b4cfcb36340c39cc3b68640e215980fbd5df
priley86 <priley86@redhat.com>
2017-08-18 18:47:57 +0000

    chore(release): updates generated by Travis build

---
+++ dist/es2015/pf-tabs/pf-tab-row-contents.template.js
@@ -0,0 +1,7 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var PfTabRowContentsTemplate = "\n<li role=\"section\">\n</li>\n";
+exports.default = PfTabRowContentsTemplate;
