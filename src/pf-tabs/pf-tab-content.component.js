8f38e3dff4d02577bda85c6b09a7b3121c52d435
Patrick Riley <priley86@gmail.com>
2017-08-18 14:44:20 -0400

    updates

--- src/pf-tabs/pf-tab-content.component.js
+++ src/pf-tabs/pf-tab-content.component.js
@@ -1,16 +1,19 @@
 import { default as tmpl } from 'pf-tab-content.template';
 
 /**
- * <b>&lt;pf-tab&gt;</b> element for Patternfly Web Components
+ * <b>&lt;pf-tab-content&gt;</b> element for Patternfly Web Components
  *
  * @example {@lang xml}
- * <pf-tabs>
- *  <pf-tab class="nav-item" content-id="content1" active="true">
+ * <pf-tabs tabs-class="nav nav-tabs">
+ *  <pf-tab tab-class="nav-item" content-id="content1" active="true">
  *    Tab One
  *  </pf-tab>
- *  <pf-tab class="nav-item" content-id="content2" active="true">
+ *  <pf-tab tab-class="nav-item" content-id="content2" active="true">
  *    Tab Two
  *  </pf-tab>
+ *  <pf-tab-row-contents contents-class="pf-tabrow-contents">
+ *    <button class="btn btn-default" type="button">Default</button>
+ *  </pf-tab-row-contents>
  * </pf-tabs>
  * <pf-tab-content content-id="content1"> <p> my content 1 </p></pf-tab-content>
  * <pf-tab-content content-id="content2"> <p> my content 2 </p></pf-tab-content>
