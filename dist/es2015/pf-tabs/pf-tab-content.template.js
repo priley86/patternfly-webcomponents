21632269efa796338c6022c7850684e9c3664577
priley86 <priley86@redhat.com>
2017-08-19 06:06:43 +0000

    chore(release): updates generated by Travis build

---
+++ dist/es2015/pf-tabs/pf-tab-content.template.js
@@ -0,0 +1,7 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var PfTabContentTemplate = "\n<div role=\"tabpanel\"></div>\n";
+exports.default = PfTabContentTemplate;
